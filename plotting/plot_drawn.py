3f5803d3f15f2cc11cc2f35a87d8147330557332
awaciern <awaciern@ncsu.edu>
2020-11-04 23:52:26 -0500

    Script to plot signatures updated to produce better visual

--- plotting/plot_drawn.py
+++ plotting/plot_drawn.py
@@ -3,25 +3,25 @@ from numpy import genfromtxt
 import matplotlib.pyplot as plot
 
 data_path = '../datasets/Task2/'
-data_file = 'U19S1.TXT'
+data_file = 'U5S25.TXT'
 
 sig_data = genfromtxt(data_path + data_file, delimiter=' ', skip_header=1, dtype=int)
 print(len(sig_data))
 
-sig_matrix = np.zeros((10000, 10000), dtype=int)
+sig_matrix = np.zeros((12000, 12000), dtype=int)
 
 for row in sig_data:
-    sig_matrix[row[0]-25:row[0]+25, row[1]-25:row[1]+25] = 1
+    sig_matrix[12000-row[1]-25:12000-row[1]+25, row[0]-25:row[0]+25] = 1
     if row[3]:
-        xdelta = row[0] - xprev
-        ydelta = row[1] - yprev
+        xdelta = 12000 - row[1] - xprev
+        ydelta = row[0] - yprev
         delta = max(abs(xdelta), abs(ydelta))
         for i in range(1, delta):
             xstep = int(i*xdelta/delta)
             ystep = int(i*ydelta/delta)
             sig_matrix[xprev+xstep-25:xprev+xstep+25, yprev+ystep-25:yprev+ystep+25] = 1
-    xprev = row[0]
-    yprev = row[1]
+    xprev = 12000 - row[1]
+    yprev = row[0]
 
 plot.imshow(sig_matrix)
 plot.show()
